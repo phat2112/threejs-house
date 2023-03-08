bed2ea9e31fb1f1414b7ce9d4c9d2900fc5b4417
ngphat <nguyengiaphat5781@gmail.com>
2023-03-08 20:39:30 +0700

    demo threejs house

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,27 +1,9 @@
 const { merge } = require("webpack-merge");
-const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
   mode: "production",
   devtool: "source-map",
-  optimization: {
-    minimizer: [
-      "...",
-      new ImageMinimizerPlugin({
-        minimizer: {
-          implementation: ImageMinimizerPlugin.imageminMinify,
-          options: {
-            plugins: [
-              ["jpegtran", { progressive: true }],
-              ["optipng", { optimizationLevel: 5 }],
-            ],
-          },
-        },
-      }),
-    ],
-  },
   plugins: [new CleanWebpackPlugin()],
 });
